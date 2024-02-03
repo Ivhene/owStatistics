2319840a82f3097b6af54a787459b928a0eeb936
Ivhene <ivarh.nesheim@gmail.com>
2024-02-04 00:31:51 +0100

    feat: added the heroes constant

---
+++ lib/constants.ts
@@ -0,0 +1,199 @@
+import { Hero } from "./types";
+
+export const Heroes: Hero[] = [
+  {
+    name: "Mauga",
+    image: "/heroImages/mauga.webp",
+    role: "tank",
+  },
+  {
+    name: "Ana",
+    image: "/heroImages/ana.webp",
+    role: "support",
+  },
+  {
+    name: "Ashe",
+    image: "/heroImages/ashe.webp",
+    role: "damage",
+  },
+  {
+    name: "Baptiste",
+    image: "/heroImages/baptiste.webp",
+    role: "support",
+  },
+  {
+    name: "Bastion",
+    image: "/heroImages/bastion.webp",
+    role: "damage",
+  },
+  {
+    name: "Brigitte",
+    image: "/heroImages/brigitte.webp",
+    role: "support",
+  },
+  {
+    name: "Cassidy",
+    image: "/heroImages/cassidy.webp",
+    role: "damage",
+  },
+  {
+    name: "D.VA",
+    image: "/heroImages/dva.webp",
+    role: "tank",
+  },
+  {
+    name: "Doomfist",
+    image: "/heroImages/doomfist.webp",
+    role: "tank",
+  },
+  {
+    name: "Echo",
+    image: "/heroImages/echo.webp",
+    role: "damage",
+  },
+  {
+    name: "Genji",
+    image: "/heroImages/genji.webp",
+    role: "damage",
+  },
+  {
+    name: "Hanzo",
+    image: "/heroImages/hanzo.webp",
+    role: "damage",
+  },
+  {
+    name: "Illari",
+    image: "/heroImages/illari.webp",
+    role: "support",
+  },
+  {
+    name: "Junker Queen",
+    image: "/heroImages/junker-queen.webp",
+    role: "tank",
+  },
+  {
+    name: "Junkrat",
+    image: "/heroImages/junkrat.webp",
+    role: "damage",
+  },
+  {
+    name: "Kiriko",
+    image: "/heroImages/kiriko.webp",
+    role: "support",
+  },
+  {
+    name: "Lifeweaver",
+    image: "/heroImages/lifeweaver.webp",
+    role: "support",
+  },
+  {
+    name: "Lucio",
+    image: "/heroImages/lucio.webp",
+    role: "support",
+  },
+  {
+    name: "Mei",
+    image: "/heroImages/mei.webp",
+    role: "damage",
+  },
+  {
+    name: "Mercy",
+    image: "/heroImages/mercy.webp",
+    role: "support",
+  },
+  {
+    name: "Moira",
+    image: "/heroImages/moira.webp",
+    role: "support",
+  },
+  {
+    name: "Orisa",
+    image: "/heroImages/orisa.webp",
+    role: "tank",
+  },
+  {
+    name: "Pharah",
+    image: "/heroImages/pharah.webp",
+    role: "damage",
+  },
+  {
+    name: "Ramattra",
+    image: "/heroImages/ramattra.webp",
+    role: "tank",
+  },
+  {
+    name: "Reaper",
+    image: "/heroImages/reaper.webp",
+    role: "damage",
+  },
+  {
+    name: "Reinhardt",
+    image: "/heroImages/reinhardt.webp",
+    role: "tank",
+  },
+  {
+    name: "Roadhog",
+    image: "/heroImages/roadhog.webp",
+    role: "tank",
+  },
+  {
+    name: "Sigma",
+    image: "/heroImages/sigma.webp",
+    role: "tank",
+  },
+  {
+    name: "Sojourn",
+    image: "/heroImages/sojourn.webp",
+    role: "damage",
+  },
+  {
+    name: "Soldier 76",
+    image: "/heroImages/soldier-76.webp",
+    role: "damage",
+  },
+  {
+    name: "Sombra",
+    image: "/heroImages/sombra.webp",
+    role: "damage",
+  },
+  {
+    name: "Symmetra",
+    image: "/heroImages/ashe.webp",
+    role: "damage",
+  },
+  {
+    name: "Torbjörn",
+    image: "/heroImages/torbjorn.webp",
+    role: "damage",
+  },
+  {
+    name: "Tracer",
+    image: "/heroImages/tracer.webp",
+    role: "damage",
+  },
+  {
+    name: "Widowmaker",
+    image: "/heroImages/widowmaker.webp",
+    role: "damage",
+  },
+  {
+    name: "Winston",
+    image: "/heroImages/winston.webp",
+    role: "tank",
+  },
+  {
+    name: "Wrecking Ball",
+    image: "/heroImages/wrecking-ball.webp",
+    role: "tank",
+  },
+  {
+    name: "Zarya",
+    image: "/heroImages/zarya.webp",
+    role: "tank",
+  },
+  {
+    name: "Zenyatta",
+    image: "/heroImages/zenyatta.webp",
+    role: "support",
+  },
+];
