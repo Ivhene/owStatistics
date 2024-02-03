f063075f86fa735ca1b364bf9e79b7361f4c9d7b
Ivhene <ivarh.nesheim@gmail.com>
2024-02-03 01:27:03 +0100

    chore: removed test page

--- app/(landing)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function LandingLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="h-full text-neutral-800 bg-slate-200 p-16">{children}</div>
-  );
-}
