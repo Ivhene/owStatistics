831c2382cdaef6ae7f986de4caf11a3924edcb97
Ivhene <ivarh.nesheim@gmail.com>
2024-05-14 15:42:36 +0200

    fix: update

--- app/(authorized)/dashboard/page.tsx
+++ app/(authorized)/dashboard/page.tsx
@@ -1,8 +1,5 @@
 import { HeroDataProcessing } from "@/components/dashboard/HeroDataProcessing";
 import { findAllGames } from "@/lib/API";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 export default async function DashboardPage() {
   const data = await findAllGames();
